b0bddf6decd3d5e83d5944f8468de7578677322b
Nishant Desai <nishantdesai1306@gmail.com>
2017-05-07 19:04:11 +0530

    change socketio'sserver url form http to https

--- src/app/shared/custom-chatzz.service.ts
+++ src/app/shared/custom-chatzz.service.ts
@@ -1,5 +1,5 @@
 import { ChatzzService, getChatzzService} from './chatzz.service';
 
-const getCustomChatzzService = (): ChatzzService => {return getChatzzService('http://localhost:3000/', true)};
+const getCustomChatzzService = (): ChatzzService => {return getChatzzService('https://localhost:3000/', true)};
 
 export default getCustomChatzzService;

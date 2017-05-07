c7bb2af25e0730bfc5b14fa349bc3f7d1dded346
Nishant Desai <nishantdesai1306@gmail.com>
2017-05-07 13:56:20 +0530

    chatzz service config changed

--- src/app/shared/custom-chatzz.service.ts
+++ src/app/shared/custom-chatzz.service.ts
@@ -1,5 +1,5 @@
 import { ChatzzService, getChatzzService} from './chatzz.service';
 
-const getCustomChatzzService = (): ChatzzService => {return getChatzzService('http://localhost:3000/')};
+const getCustomChatzzService = (): ChatzzService => {return getChatzzService('http://localhost:4200/')};
 
 export default getCustomChatzzService;

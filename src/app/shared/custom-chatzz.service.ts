87f35c9a946ed039b4e21857b98560d6f14275c1
Nishant Desai <nishantdesai1306@gmail.com>
2017-05-07 19:56:11 +0530

    updated dist

--- src/app/shared/custom-chatzz.service.ts
+++ src/app/shared/custom-chatzz.service.ts
@@ -1,5 +1,5 @@
 import { ChatzzService, getChatzzService} from './chatzz.service';
 
-const getCustomChatzzService = (): ChatzzService => {return getChatzzService('https://localhost:3000/', true)};
+const getCustomChatzzService = (): ChatzzService => {return getChatzzService('https://lost-box.herokuapp.com/', true)};
 
 export default getCustomChatzzService;
